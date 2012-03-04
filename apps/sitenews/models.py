2e59b4c627f327715deae09846fb17779accda02
Will Kahn-Greene <willg@bluesock.org>
2012-03-04 12:17:20 -0500

    Fix ordering of site news
    Site news should be ordered from most recent on top to least recent on
    bottom. Somehow I inverted that.

--- apps/sitenews/models.py
+++ apps/sitenews/models.py
@@ -37,7 +37,7 @@ class SiteNews(models.Model):
 
     class Meta(object):
         get_latest_by = "updated"
-        ordering = ["-updated"]
+        ordering = ["updated"]
 
     @models.permalink
     def get_absolute_url(self):
