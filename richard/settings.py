9b674d0368af2e73ea8db8fd543009c3b95b2196
Reiner Gerecke <mr.squidd@gmail.com>
2012-04-18 22:49:37 +0200

    Change default SQLite database name
    richard.db clashes with the richard directory on tab-completion.

--- richard/settings.py
+++ richard/settings.py
@@ -33,7 +33,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',  # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': os.path.join(SITE_ROOT, 'richard.db'),  # Or path to database file if using sqlite3.
+        'NAME': os.path.join(SITE_ROOT, 'database.db'),  # Or path to database file if using sqlite3.
         'USER': '',  # Not used with sqlite3.
         'PASSWORD': '',  # Not used with sqlite3.
         'HOST': '',  # Set to empty string for localhost. Not used with sqlite3.
