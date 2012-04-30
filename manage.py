b8a29b24739e66e728c3cd60b2cd27cccd029879
Will Kahn-Greene <willg@bluesock.org>
2012-04-30 02:09:19 -0400

    Remove superfluous import

--- manage.py
+++ manage.py
@@ -16,7 +16,7 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import os, sys, site
+import os, sys
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "richard.settings")
