418934438dda2d6e2b64b487de84fccaa053124d
Reiner Gerecke <mr.squidd@gmail.com>
2012-05-09 11:14:00 +0200

    89. Move most JS code into its own file

---
+++ apps/videos/static/videos/js/videos.js
@@ -0,0 +1,45 @@
+// Parse the hash at the end of the URL and return it as object.
+// Example: '#t=3m&foo=bar' returns {t: '3m', foo: 'bar'}
+// TODO: error handling
+function parseLocationHash() {
+  var parts = window.location.hash.slice(1).split('&'),
+	  result = {};
+
+  for (var i = 0, length = parts.length; i < length; i++) {
+	var tuple = parts[i].split('=');
+	result[tuple[0]] = tuple[1];
+  }
+
+  return result;
+}
+
+// Parse time string and return the value in seconds.
+// Example: '3m16s' returns 196
+// TODO: error handling
+function parseTime(value) {
+  var time = 0;
+
+  var minutes = value.split('m');
+  var seconds = minutes.slice(-1)[0].split('s');
+
+  time = minutes.length > 1 ? parseInt(minutes[0]) * 60 : 0;
+  time += seconds.length > 1 ? parseInt(seconds[0]) : 0;
+
+  return time;
+}
+
+// Seek to specified time in the video and start playing.
+function UnisubsSeekVideo(time) {
+  // We need to wait until the player is initialized before we can set
+  // a time. This just checks every second if a certain global is
+  // defined.
+  // FIXME: this does not mean the player/video is ready! I hope we
+  //        find a better way to do this.
+  if (typeof(unisubs) === "undefined") {
+	setTimeout(function() { UnisubsSeekVideo(time); }, 1000);
+	return;
+  }
+
+  var w = unisubs.widget.Widget.getAllWidgets()[0];
+  w.playAt(time);
+}
