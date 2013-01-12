6abaebc95e565176c0b57ae64dfc47c96c666501
Will Kahn-Greene <willg@bluesock.org>
2013-01-12 14:42:35 -0500

    Fix videoreqs command
    This fixes some goofy things with "null" and adds a "default" key
    that denotes whether the field has a default set or not.

--- richard/videos/management/commands/videoreqs.py
+++ richard/videos/management/commands/videoreqs.py
@@ -15,10 +15,9 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import json
-import os
 
-from django.conf import settings
-from django.core.management.base import BaseCommand, CommandError
+from django.db.models import fields
+from django.core.management.base import BaseCommand
 
 from richard.videos.models import Video
 
@@ -28,7 +27,7 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         # Generate the basic stuff
-        fields = []
+        reqs = []
         for field in Video._meta.fields:
             # Skip some things that shouldn't be in an API push
             if field.name in ['id', 'updated']:
@@ -37,6 +36,7 @@ class Command(BaseCommand):
             data = {
                 'name': field.name,
                 'type': field.get_internal_type(),
+                'has_default': field.default is not fields.NOT_PROVIDED,
                 'null': field.null,
                 'empty_strings': field.empty_strings_allowed,
                 'choices': [mem[0] for mem in field.choices],
@@ -48,6 +48,7 @@ class Command(BaseCommand):
                         'type': 'TextField',
                         'empty_strings': False,
                         'null': False,
+                        'has_default': False,
                         'choices': [],
                         'html': False
                         })
@@ -56,33 +57,36 @@ class Command(BaseCommand):
                         'type': 'TextField',
                         'empty_strings': False,
                         'null': False,
+                        'has_default': False,
                         'choices': [],
                         'html': False
                         })
 
-            fields.append(data)
+            reqs.append(data)
 
         # Add tags and speakers which are M2M, but we do them funkily
         # in the API.
-        fields.append({
+        reqs.append({
                 'name': 'tags',
                 'type': 'TextArrayField',
                 'empty_strings': False,
-                'null': False,
+                'null': True,
+                'has_default': False,
                 'choices': [],
                 'html': False
                 })
-        fields.append({
+        reqs.append({
                 'name': 'speakers',
                 'type': 'TextArrayField',
                 'empty_strings': False,
-                'null': False,
+                'has_default': False,
+                'null': True,
                 'choices': [],
                 'html': False
                 })
 
         f = open('video_reqs.json', 'w')
-        f.write(json.dumps(fields, indent=2))
+        f.write(json.dumps(reqs, indent=2))
         f.close()
 
         self.stdout.write('Done!\n')
