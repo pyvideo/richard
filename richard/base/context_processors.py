62434b2de963162ada7f2e4d8f26fa0451cf2f26
codersquid <github@codersquid.com>
2013-12-28 21:50:58 -0600

    fixes static urls, comments, and pluralize on the home page

--- richard/base/context_processors.py
+++ richard/base/context_processors.py
@@ -13,6 +13,7 @@
 #
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
+from django.conf import settings
 from richard.sitenews.models import Notification
 
 
@@ -21,5 +22,6 @@ def base(request):
     notifications = Notification.get_live_notifications()
 
     return {
+        'settings': settings,
         'notifications': notifications,
         }
