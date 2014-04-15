b3fb08c41ff2428ddb0be1e0425ce63660732d0f
westurner <wes.turner@gmail.com>
2014-04-14 21:08:18 -0500

    Add schema.org VideoObject RDFa metadata
    * Update video.html: http://schema.org/VideoObject
    * Add duration_iso8601 filter and tests
    * Add duration_iso8601 filter and tests
    * Updated test_filters.py: PEP8
    * Updated video.html: next <meta> in <dd>
    * Removed explicit Python tag
    * Updated video.html: Added v.category.title tag
    * Updated video.html: Added sameAs v.get_absolute_url
    * Updated video.html: <meta content=> -> <link href=>
    
    See https://github.com/willkg/richard/pull/213

--- richard/base/templatetags/duration.py
+++ richard/base/templatetags/duration.py
@@ -18,6 +18,15 @@ from django import template
 
 register = template.Library()
 
+def seconds_to_hms(duration):
+    """Convert seconds to hours, minutes, seconds.
+    Represents times over one day as hours > 24.
+    """
+    duration = int(duration)
+    hours, _t = divmod(duration, 3600)
+    minutes, seconds = divmod(_t, 60)
+    return hours, minutes, seconds
+
 
 @register.filter
 def duration(duration):
@@ -26,10 +35,7 @@ def duration(duration):
     if duration is None:
         return ''
 
-    duration = int(duration)
-    seconds = duration % 60
-    minutes = (duration // 60) % 60
-    hours = (duration // 60) // 60
+    hours, minutes, seconds = seconds_to_hms(duration)
 
     s = '%02d' % (seconds)
     m = '%02d' % (minutes)
@@ -41,3 +47,16 @@ def duration(duration):
     output.append(m)
     output.append(s)
     return ':'.join(output)
+
+
+@register.filter
+def duration_iso8601(duration):
+    """Filter that converts a duration in seconds to ISO8601 duration format
+    like T01H01M01S. Represents times over one day as hours > 24.
+    """
+    if not duration:
+        hours = minutes = seconds = 0
+    else:
+        hours, minutes, seconds = seconds_to_hms(duration)
+    return 'PT%02dH%02dM%02dS' % (hours, minutes, seconds)
+
--- richard/base/tests/test_filters.py
+++ richard/base/tests/test_filters.py
@@ -19,7 +19,10 @@ import unittest
 from nose.tools import eq_
 
 from richard.base.templatetags.batch import batch
+
+from richard.base.templatetags.duration import seconds_to_hms
 from richard.base.templatetags.duration import duration
+from richard.base.templatetags.duration import duration_iso8601
 
 
 class BatchFilterTest(unittest.TestCase):
@@ -34,6 +37,29 @@ class BatchFilterTest(unittest.TestCase):
 
     def test_padwith(self):
         eq_(batch([1, 2, 3, 4, 5], '2,FOO'), [[1, 2], [3, 4], [5, 'FOO']])
+
+
+class Seconds_to_hmsTest(unittest.TestCase):
+    def test_seconds(self):
+        eq_((0, 0, 15), seconds_to_hms(15))
+        eq_((0, 0, 1),  seconds_to_hms(1))
+
+    def test_minutes(self):
+        eq_((0, 1, 1), seconds_to_hms(61))
+        eq_((0, 1, 0), seconds_to_hms(60))
+        eq_((0, 2, 0), seconds_to_hms(120))
+
+    def test_hours(self):
+        eq_((1, 0, 0), seconds_to_hms(3600))
+        eq_((1, 0, 2), seconds_to_hms(3602))
+        eq_((2, 0, 0), seconds_to_hms(7200))
+        eq_((2, 2, 0), seconds_to_hms(7320))
+        eq_((2, 2, 1), seconds_to_hms(7321))
+        eq_((2, 2, 2), seconds_to_hms(7322))
+
+    def test_days(self):
+        # NOTE: Represents times greater than one day as more than 24 hours
+        eq_((25, 0, 1), seconds_to_hms(90001))
 
 
 class DurationFilterTest(unittest.TestCase):
@@ -56,3 +82,25 @@ class DurationFilterTest(unittest.TestCase):
 
     def test_bad(self):
         eq_('', duration(None))
+
+
+class DurationISO8601FilterTest(unittest.TestCase):
+    def test_seconds(self):
+        eq_("PT00H00M00S", duration_iso8601(0))
+        eq_("PT00H00M01S", duration_iso8601(1))
+
+    def test_minutes(self):
+        eq_("PT00H01M01S", duration_iso8601(61))
+        eq_("PT00H01M00S", duration_iso8601(60))
+        eq_("PT00H02M00S", duration_iso8601(120))
+
+    def test_hours(self):
+        eq_("PT01H00M00S", duration_iso8601(3600))
+        eq_("PT01H00M02S", duration_iso8601(3602))
+        eq_("PT02H00M00S", duration_iso8601(7200))
+        eq_("PT02H02M00S", duration_iso8601(7320))
+        eq_("PT02H02M01S", duration_iso8601(7321))
+        eq_("PT02H02M02S", duration_iso8601(7322))
+
+    def test_bad(self):
+        eq_("PT00H00M00S", duration_iso8601(None))
