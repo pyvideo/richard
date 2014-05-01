1076199d638fa629aeb02978aec1de3afdb38e60
Will Kahn-Greene <willkg@bluesock.org>
2014-05-01 17:42:07 -0400

    Update django version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 requirements = [
     'bleach==1.4',
-    'django==1.6.2',
+    'django==1.6.4',
     'djangorestframework==2.3.10',
     'django-browserid==0.10',
     'django-grappelli==2.5.2',
