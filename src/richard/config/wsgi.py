6c4e6303c3e5b6b3f9e6c62347a323268ceef354
Sheila Miguez <github@codersquid.com>
2015-03-07 12:19:07 -0600

    Fix wsgi to use django-configurations framework

--- src/richard/config/wsgi.py
+++ src/richard/config/wsgi.py
@@ -36,7 +36,7 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "richard.settings")
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
 # setting points here.
-from django.core.wsgi import get_wsgi_application
+from configurations.wsgi import get_wsgi_application
 application = get_wsgi_application()
 
 # Apply WSGI middleware here.
